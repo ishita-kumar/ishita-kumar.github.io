60f1a756a0e40b601044d3808be6c9b6accba48d
Ishita Kumar <ishitakumar712@gmail.com>
2020-01-02 22:11:35 -0500

    lala

--- script.js
+++ script.js
@@ -20,9 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-$('.trigger').click(function() {
-    $(this).parents('.page-about').toggleClass('show-info');
-  });
+
 
 
 ! function($) {
